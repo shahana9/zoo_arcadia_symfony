1c26406e4bb68e69835fd13a7528db2ae5990ccd
shahana <shahana93@hotmail.com>
2024-05-24 00:21:16 +0200

    installation encore package

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,17 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JavaScript
+import 'bootstrap';
+
+// You can also import Popper.js if you need it for tooltips and popovers
+import '@popperjs/core';
 import './styles/app.css';
+import '../templates/common_front/style.css';
+import '../templates/home/style.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
