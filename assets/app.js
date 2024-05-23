3de81c664958ecfc4cfd48342d8eec4e19d43e85
shahana <shahana93@hotmail.com>
2024-05-24 00:29:11 +0200

    remove the unrequired data

--- assets/app.js
+++ assets/app.js
@@ -16,6 +16,5 @@ import 'bootstrap';
 import '@popperjs/core';
 import './styles/app.css';
 import '../templates/common_front/style.css';
-import '../templates/home/style.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
