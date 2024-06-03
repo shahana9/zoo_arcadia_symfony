f0266116c8e4d40422fed186e759774769cb9839
shahana <shahana93@hotmail.com>
2024-06-03 13:51:05 +0200

    save LoginFormAuthenticator

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,7 +50,6 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('home'));
-        
     }
 
     protected function getLoginUrl(Request $request): string
