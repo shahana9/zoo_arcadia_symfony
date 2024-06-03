2bdeec56825bcf71bf5e29b3038429af5c50edb3
shahana <shahana93@hotmail.com>
2024-06-03 10:14:24 +0200

    reset password config

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -48,7 +48,7 @@ class ResetPasswordController extends AbstractController
             );
         }
 
-        return $this->render('reset_password/request.html.twig', [
+        return $this->render('reset_password/request_form/request.html.twig', [
             'requestForm' => $form,
         ]);
     }
