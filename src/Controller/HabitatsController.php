bdf734c97b021f543f2c0de1892e1143758e89a9
shahana <shahana93@hotmail.com>
2024-06-03 08:29:15 +0200

    configuration of easyadmin for admin user

--- src/Controller/HabitatsController.php
+++ src/Controller/HabitatsController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
 class HabitatsController extends AbstractController
 {
     #[Route('/habitats', name: 'app_habitats')]
@@ -16,11 +14,5 @@ class HabitatsController extends AbstractController
             'controller_name' => 'HabitatsController',
             'title' => 'Habitats',
         ]);
-    }
-    #[Route('/habitat/edit', name: 'habitat_edit')]
-    #[IsGranted('ROLE_ADMIN')]
-    public function edit(): Response
-    {
-        return $this->render('habitat/edit.html.twig');
     }
 }
--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ServicesController extends AbstractController
 {
@@ -15,11 +14,5 @@ class ServicesController extends AbstractController
         return $this->render('services/services.html.twig', [
             'title' => 'Services',
         ]);
-    }
-    #[Route('/service/edit', name: 'service_edit')]
-    #[IsGranted('ROLE_ADMIN')]
-    public function edit(): Response
-    {
-        return $this->render('service/edit.html.twig');
     }
 }
