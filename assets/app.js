c0f09091d7c6d2b964758ab44ba82994a34bcd54
shahana <shahana93@hotmail.com>
2024-06-10 14:27:47 +0200

    transfer css files in css in  assets

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,7 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+import './bootstrap';
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import Bootstrap JavaScript
-import 'bootstrap';
-
-// You can also import Popper.js if you need it for tooltips and popovers
-import '@popperjs/core';
 import './styles/app.css';
 import '../assets/css/header_footer.css';
 import '../assets/css/home.css';
@@ -22,5 +10,18 @@ import '../assets/css/habitats.css';
 import '../assets/css/contact.css';
 import '../assets/css/login.css';
 import '../assets/css/request.css';
+
+// Import Bootstrap JavaScript
+import 'bootstrap';
+// You can also import Popper.js if you need it for tooltips and popovers
+import '@popperjs/core';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
