21ec2df71fda0fb943abb2865836a19112a422cf
shahana <shahana93@hotmail.com>
2024-06-23 23:01:40 +0200

    import class and add role type

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var list<string> The user roles
      */
-    #[ORM\Column]
+    #[ORM\Column(type:"json")]
     private array $roles = [];
 
     /**
@@ -67,7 +67,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = ['ROLE_USER'];
 
         return array_unique($roles);
     }
