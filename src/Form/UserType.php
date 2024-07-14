80ac69b078dd80834a0e92c693bd1de84321a5a4
shahana <shahana93@hotmail.com>
2024-07-14 18:44:51 +0200

    role config update

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -24,14 +24,14 @@ class UserType extends AbstractType
                 'Employé' => 'ROLE_STAFF',
 
                 ],
-            'multiple' => true, // Permet une seule sélection
-            'expanded' => false, // Utilise une liste déroulante
+            'multiple' => true, 
+            'expanded' => false, 
             'choice_label' => function ($choice, $key, $value) {
-                // Utilisez cette fonction pour définir le label des options
+                
                 return $key;
             },
             'choice_value' => function ($choice) {
-                // Utilisez cette fonction pour définir la valeur de l'option
+               
                 return $choice;
             },
             ])
