2c001698603f405f1eb04ae632b47554a4177c03
shahana <shahana93@hotmail.com>
2024-07-15 18:02:01 +0200

    contact form resolv error

---
+++ src/Form/UserType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                'Vétérinaire' => 'ROLE_DOCTEUR',
+                'Employé' => 'ROLE_STAFF',
+
+                ],
+            'multiple' => true, 
+            'expanded' => false, 
+            'choice_label' => function ($choice, $key, $value) {
+                
+                return $key;
+            },
+            'choice_value' => function ($choice) {
+               
+                return $choice;
+            },
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            
+        ]);
+    }
+}
