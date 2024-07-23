728aa44f98d6e14a9f2449d8379288082e07b8df
shahana <shahana93@hotmail.com>
2024-07-23 16:08:59 +0200

    Configure Symfony to ignore .env in production

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,15 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__) . '/.env')) {
+// Load cached env vars if the .env.local.php file exists
+// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
+if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
+    foreach ($env as $k => $v) {
+        $_ENV[$k] = $_SERVER[$k] = $v;
+    }
+} elseif (!class_exists(Dotenv::class)) {
+    throw new \RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+} else {
+    // Load all the .env files
     (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
 }
