52cab141966619c15c9497c52b691c901df69a1a
shahana <shahana93@hotmail.com>
2024-07-23 16:35:24 +0200

    Add .env to .gitignore

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -13,6 +13,6 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
 } elseif (!class_exists(Dotenv::class)) {
     throw new \RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
-    // Load all the .env files
-    (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
+    $dotenv = new Dotenv();
+    $dotenv->bootEnv(dirname(__DIR__) . '/.env');
 }
