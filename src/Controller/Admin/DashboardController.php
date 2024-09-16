200c8e4d11c1bbedcc673382c19c351fe52e9bbc
shahana <shahana93@hotmail.com>
2024-09-16 14:50:12 +0200

    resolve login issue

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Avis;
 use App\Entity\User;
@@ -14,34 +16,46 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class DashboardController extends AbstractController
+class DashboardController extends AbstractDashboardController
 {
-    private AuthorizationCheckerInterface $authChecker;
-    private AdminUrlGenerator $adminUrlGenerator;
 
-    public function __construct(AuthorizationCheckerInterface $authChecker, AdminUrlGenerator $adminUrlGenerator)
-    {
-        $this->authChecker = $authChecker;
-        $this->adminUrlGenerator = $adminUrlGenerator;
-    }
-
-    #[Route('/admin', name: 'admin')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+
         if ($this->isGranted('ROLE_ADMIN')) {
-            $url = $this->adminUrlGenerator->setController(UserCrudController::class)->generateUrl();
+            $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+            $url = $adminUrlGenerator->setController(UserCrudController::class)->generateUrl();
             return $this->redirect($url);
-        } elseif ($this->isGranted('ROLE_VETERINAIRE')) {
-            return $this->render('veterinarian/dashboard.html.twig');
-        } elseif ($this->isGranted('ROLE_EMPLOYE')) {
-            return $this->render('employee/dashboard.html.twig');
-        } else {
-            throw $this->createAccessDeniedException();
         }
+
+        if ($this->isGranted('ROLE_VETERINAIRE')) {
+            return $this->render('veterinarian/dashboard.html.twig');
+        }
+        
+        if ($this->isGranted('ROLE_EMPLOYE')) {
+            return $this->render('employee/dashboard.html.twig');
+        }
+
+        throw $this->createAccessDeniedException();
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
     }
+
 
     public function configureDashboard(): Dashboard
     {
@@ -52,7 +66,9 @@ class DashboardController extends AbstractController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        // yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+        yield MenuItem::linkToLogout('Se déconnecter', 'fas fa-power-off');
     }
+
 }
